a34a38030eba36a7ddd6f1db6e746c2b587529fc
Naman Jain <161619333+Namanjain122@users.noreply.github.com>
2026-05-28 15:32:33 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,14 +1,23 @@
 from dotenv import load_dotenv
 import mysql.connector
+import streamlit as st
 import os
 
+# Load environment variables
 load_dotenv()
 
+
 def get_connection():
+    host = os.getenv("DB_HOST") or st.secrets["DB_HOST"]
+    user = os.getenv("DB_USER") or st.secrets["DB_USER"]
+    password = os.getenv("DB_PASSWORD") or st.secrets["DB_PASSWORD"]
+    database = os.getenv("DB_NAME") or st.secrets["DB_NAME"]
+    port = os.getenv("DB_PORT") or st.secrets["DB_PORT"]
 
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=int(port)
     )
